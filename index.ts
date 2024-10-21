bb68d8be6d7c45011739c09a50b5b5cab3fc5719
Dasha2203 <44381104+Dasha2203@users.noreply.github.com>
2024-10-21 23:51:56 +0300

    feat: 500 erros

--- index.ts
+++ index.ts
@@ -13,12 +13,120 @@ const headers = {
 }
 
 const server = createServer(async (req, res) => {
-  const method = req.method;
+  try {
+    const method = req.method;
 
-  if (method === 'PUT' && req.url?.includes('/api/users')) {
-    const partsPath = req.url.split('/').filter(i => !!i);
+    if (method === 'PUT' && req.url?.includes('/api/users')) {
+      const partsPath = req.url.split('/').filter(i => !!i);
 
-    if (partsPath.length === 3) {
+      if (partsPath.length === 3) {
+        const userId = partsPath[2];
+
+        if (!validate(userId)) {
+          res.writeHead(400, headers);
+          res.end(JSON.stringify({ message: `${userId} is invalid` }));
+
+          return;
+        }
+
+        const user = db.findUser(userId);
+
+        if (!user) {
+          res.writeHead(404, headers);
+          res.end(JSON.stringify({ message: `${userId} didn't find` }));
+
+          return;
+        }
+
+        const body = await getBody(req);
+        const data = JSON.parse(body);
+
+        if (!data.username || !data.age || !Array.isArray(data.hobbies)) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ message: 'Incorrect data format' }));
+
+          return;
+        }
+
+        const updatedUser: User = {
+          id: userId,
+          username: data.username,
+          age: data.age,
+          hobbies: data.hobbies,
+        };
+
+        db.updateUser(updatedUser);
+        res.writeHead(200, headers);
+        res.end(JSON.stringify(updatedUser));
+
+        return;
+      }
+    }
+
+    if (method === 'GET') {
+      if (req.url?.includes('/api/users')) {
+        const partsPath = req.url.split('/').filter(i => !!i);
+
+        if (partsPath.length === 3) {
+          const userId = partsPath[2];
+
+          if (!validate(userId)) {
+            res.writeHead(400, headers);
+            res.end(JSON.stringify({ message: `${userId} is invalid` }));
+
+            return;
+          }
+
+          const user = db.findUser(userId);
+
+          if (!user) {
+            res.writeHead(404, headers);
+            res.end(JSON.stringify({ message: `${userId} didn't find` }));
+    
+            return;
+          }
+
+          res.writeHead(200, headers);
+          res.end(JSON.stringify(user));
+
+          return;
+        }
+
+        const users = db.getUsers();
+        res.writeHead(200, headers);
+        res.end(JSON.stringify(users));
+
+        return;
+      }
+    }
+
+    if (req.method === 'POST' && req.url?.includes('/api/users')) {
+      const body = await getBody(req);
+      const data = JSON.parse(body);
+
+      if (!data.username || !data.age || !Array.isArray(data.hobbies)) {
+        res.writeHead(400, headers);
+        res.end(JSON.stringify({ message: 'Incorrect data format' }));
+        return;
+      }
+
+      const newUser: User = {
+        id: uuidv4(),
+        username: data.username,
+        age: data.age,
+        hobbies: data.hobbies
+      }
+
+      db.addUser(newUser);
+      res.writeHead(200, headers);
+      res.end(JSON.stringify(newUser));
+
+      return;
+    }
+
+    if (req.method === 'DELETE' && req.url?.includes('/api/users')) {
+      const partsPath = req.url.split('/').filter(i => !!i);
+
       const userId = partsPath[2];
 
       if (!validate(userId)) {
@@ -37,123 +145,24 @@ const server = createServer(async (req, res) => {
         return;
       }
 
-      const body = await getBody(req);
-      const data = JSON.parse(body);
-
-      if (!data.username || !data.age || !Array.isArray(data.hobbies)) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ message: 'Incorrect data format' }));
-
-        return;
-      }
-
-      const updatedUser: User = {
-        id: userId,
-        username: data.username,
-        age: data.age,
-        hobbies: data.hobbies,
-      };
-
-      db.updateUser(updatedUser);
-      res.writeHead(200, headers);
-      res.end(JSON.stringify(updatedUser));
-
+      db.deleteUser(userId);
+      res.writeHead(204, headers);
+      res.end();
       return;
     }
+
+    res.writeHead(404, headers);
+    res.end(JSON.stringify({
+      message: `The path ${req.url} didn't find`,
+    }))
+
+  } catch(err) {
+    res.writeHead(500, headers);
+    res.end(JSON.stringify({
+      message: 'There was an error on the server'
+    }))
   }
-
-  if (method === 'GET') {
-    if (req.url?.includes('/api/users')) {
-      const partsPath = req.url.split('/').filter(i => !!i);
-
-      if (partsPath.length === 3) {
-        const userId = partsPath[2];
-
-        if (!validate(userId)) {
-          res.writeHead(400, headers);
-          res.end(JSON.stringify({ message: `${userId} is invalid` }));
-
-          return;
-        }
-
-        const user = db.findUser(userId);
-
-        if (!user) {
-          res.writeHead(404, headers);
-          res.end(JSON.stringify({ message: `${userId} didn't find` }));
   
-          return;
-        }
-
-        res.writeHead(200, headers);
-        res.end(JSON.stringify(user));
-
-        return;
-      }
-
-      const users = db.getUsers();
-      res.writeHead(200, headers);
-      res.end(JSON.stringify(users));
-
-      return;
-    }
-  }
-
-  if (req.method === 'POST' && req.url?.includes('/api/users')) {
-    const body = await getBody(req);
-    const data = JSON.parse(body);
-
-    if (!data.username || !data.age || !Array.isArray(data.hobbies)) {
-      res.writeHead(400, headers);
-      res.end(JSON.stringify({ message: 'Incorrect data format' }));
-      return;
-    }
-
-    const newUser: User = {
-      id: uuidv4(),
-      username: data.username,
-      age: data.age,
-      hobbies: data.hobbies
-    }
-
-    db.addUser(newUser);
-    res.writeHead(200, headers);
-    res.end(JSON.stringify(newUser));
-
-    return;
-  }
-
-  if (req.method === 'DELETE' && req.url?.includes('/api/users')) {
-    const partsPath = req.url.split('/').filter(i => !!i);
-
-    const userId = partsPath[2];
-
-    if (!validate(userId)) {
-      res.writeHead(400, headers);
-      res.end(JSON.stringify({ message: `${userId} is invalid` }));
-
-      return;
-    }
-
-    const user = db.findUser(userId);
-
-    if (!user) {
-      res.writeHead(404, headers);
-      res.end(JSON.stringify({ message: `${userId} didn't find` }));
-
-      return;
-    }
-
-    db.deleteUser(userId);
-    res.writeHead(204, headers);
-    res.end();
-    return;
-  }
-
-  res.writeHead(404, headers);
-  res.end(JSON.stringify({
-    message: `The path ${req.url} didn't find`,
-  }))
 })
 
 server.listen(PORT, () => {
